7950165acdb1248eef71b8463e97caac6356b99c
Mudassarassi <bcnxico@live.com>
2021-01-20 19:20:32 +0100

    add php

---
+++ styles/footer.php
@@ -0,0 +1,6 @@
+<?php echo '<footer><p>Copyright &#169; 2020 </p>
+<p><a href="Contacto">Contacto</p>
+  <a href="https://twitter.com"> <img src="images/twitter.jpeg" alt="twitter" width="40" height="40"/></a>
+  <a href="https://instagram.com">     <img src="images/instagram.jpeg" alt="instagram" width="40" height="40"/></a>
+  <a href="https://facebook.com"> <img src="images/facebook.jpeg" alt="facebook" width="40" height ="40" /></a></footer>';
+?>
