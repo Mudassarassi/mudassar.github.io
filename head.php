4d0b72ce5ff711be7d6f182508c368de42bf1f68
Mudassarassi <bcnxico@live.com>
2021-01-23 18:33:15 +0100

    agregar comentario y php de cabezera

---
+++ head.php
@@ -0,0 +1,39 @@
+<?php
+echo ('<meta charset="UTF-8"> <!-- selecionamo idioma  -->
+<meta name="Descripcion" content="PHP page">
+<meta name="author" content="Mudassar Assi"> <!-- aqui pondemos nombre de uathor  -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+<title>Mudassar Assi PHP curso </title> <!-- titulo de pagina -->
+<link rel="icon" type="icon/png" href="images/gato.png">
+<style> <!-- aqui sera el diseno de la pagina general -->
+.menu{
+  background: #ccc;
+  padding: 5px 15px;
+  margin-top: 0px;
+
+
+}
+.menu li {
+  list-style: none;
+  font: bold 14px verdana;
+  display: inline-block;
+  padding: 5px;
+
+}
+header, h1, ul, img{
+  display: block;
+
+}
+.cabezera{
+  background-image: url('images/phpback.jpeg');
+}
+.icono{ text-align: center; }
+.icono img {
+  border: 0.2px solid black;
+  display: inline-block;
+}
+img{margin: auto;}
+</style>')
+?>
